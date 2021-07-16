df7416252eb4ab29261fb3f237bae9b9b04a96d9
psilverby <patrik@silverby.se>
2021-07-16 22:38:12 +0200

    Logic for the task created.

--- Skocimis/Program.cs
+++ Skocimis/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Skocimis
 {
@@ -6,7 +8,49 @@ namespace Skocimis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Input
+            //Three integers A, B and C (0 < A < B < C < 100), the intial position of the kangaroos
+            string input = Console.ReadLine();
+
+            string[] positions = input.Split(" ");
+
+            int positionLeft = int.Parse(positions[0]);
+            int positionMiddle = int.Parse(positions[1]);
+            int positionRight = int.Parse(positions[2]);
+
+            int leftGap;
+            int rightGap;
+            int numberOfJumps = 0;
+            int newMiddle;
+            do
+            {
+
+                leftGap = positionMiddle - positionLeft;
+                rightGap = positionRight - positionMiddle;
+
+                if(leftGap == 1 && rightGap == 1)
+                {
+                    continue;
+                }
+
+
+                if (rightGap > leftGap)
+                {
+                    newMiddle = positionMiddle + 1;
+                    positionLeft = positionMiddle;  
+                }
+                else
+                {
+                    newMiddle = positionMiddle - 1;
+                    positionRight = positionMiddle;
+                }
+
+                positionMiddle = newMiddle;
+                numberOfJumps++;
+
+            } while (leftGap > 1 || rightGap > 1);
+
+            Console.WriteLine(numberOfJumps);
         }
     }
 }
