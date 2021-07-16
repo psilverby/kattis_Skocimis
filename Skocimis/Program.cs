699b8f7e2ce396e36a6e56623b914f245058a0c4
psilverby <87539667+psilverby@users.noreply.github.com>
2021-07-16 18:48:42 +0200

    Add files via upload

---
+++ Skocimis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Skocimis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
